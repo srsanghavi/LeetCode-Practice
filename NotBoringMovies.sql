7827c43e9db57686a068b0523f9eefc4a82ef682
Shashwat Sanghavi <shashwatsanghavi@Shashwats-MacBook-Pro.local>
2018-09-25 22:58:24 -0400

    add Not Boring Movies SQL query

---
+++ NotBoringMovies.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/not-boring-movies/description/
+
+# Write your MySQL query statement below
+SELECT *
+FROM cinema
+WHERE id%2=1 and description <> 'boring'
+ORDER BY rating DESC
