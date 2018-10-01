120829ca4c9e3f74ca49e2c0f78573a21844ef47
srsanghavi <srs.shashwat@gmail.com>
2018-09-30 21:00:05 -0400

    add solution for Merge Binary Trees

---
+++ MergeBinaryTrees.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/merge-two-binary-trees/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) return null;
+        if(t1 == null) return t2;
+        if(t2 == null) return t1;
+        TreeNode m = new TreeNode(t1.val+t2.val);
+        m.left = mergeTrees(t1.left,t2.left);
+        m.right = mergeTrees(t1.right,t2.right);
+        return m;
+        
+    }
+}
